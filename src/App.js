ea39dc21261378f6615180aa12bd36349170f8fc
dnrgus1127 <dnrgus1127@naver.com>
2022-08-05 20:56:25 +0900

    0805

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
-
-
-function Article(props) {
-  return <article>
-    <h2>{props.title}</h2>
-    {props.body}
-  </article>
-}
 
 
 function Header(props) { //Component
   console.log('props', props, props.title)
   return <header>
-    <h1><a href='/' onClick={function(event){
+    <h1><a href='/' onClick={function (event) {
       event.preventDefault();
       props.onChangeMode();
     }}>{props.title}</a></h1>
@@ -22,43 +15,71 @@ function Header(props) { //Component
 }
 
 
-function Nav(props) {
-  const lis = []
-  for (let i = 0; i < props.topics.length; i++) {
-    let t = props.topics[i];
-    lis.push(<li key={t.id}>
-      <a id={t.id} href={'/read/' + t.id} onClick={(event)=>{
-        event.preventDefault();
-        props.onChangeMode(event.target.id);
-      }}>{t.title}</a>
-      </li>)
-  }
-  return <nav>
-    <ol>
-      {lis}
-    </ol>
-  </nav>
+function HeadTitle(props) {
+  return <h1 className='HeadTitle'>
+    {props.title}
+  </h1>
+
 }
+
+function TItleImg() {
+  return <img src="/images/universe.jpg" alt='house' width="98.5%" height="30%" className='Center'></img>
+}
+
+function Menu() {
+  return <div id="menu">
+    <ul>
+      <li><a href='/'>Menu1</a></li>
+      <li><a href='/'>Menu2</a></li>
+      <li><a href='/'>Menu3</a></li>
+      <li><a href='/'>Menu4</a></li>
+    </ul>
+  </div>
+}
+
+// function Nav(props) {
+//   const lis = []
+//   for (let i = 0; i < props.topics.length; i++) {
+//     let t = props.topics[i];
+//     lis.push(<li key={t.id}>
+//       <a id={t.id} href={'/read/' + t.id} onClick={(event) => {
+//         event.preventDefault();
+//         props.onChangeMode(event.target.id);
+//       }}>{t.title}</a>
+//     </li>)
+//   }
+//   return <nav>
+//     <ol>
+//       {lis}
+//     </ol>
+//   </nav>
+// }
+
 
 
 function App() {
-  const topics = [
-    { id: 1, title: 'html', body: 'html is ...' },
-    { id: 2, title: 'vue', body: 'vue is ...' },
-    { id: 3, title: 'js', body: 'js is ...' },
-  ]
+  // const topics = [
+  //   { id: 1, title: 'html', body: 'html is ...' },
+  //   { id: 2, title: 'vue', body: 'vue is ...' },
+  //   { id: 3, title: 'js', body: 'js is ...' },
+  // ]
+  const [message, setMessage] = useState(""); useEffect(() => { fetch('/api/hello').then(response => response.text()).then(message => { setMessage(message); }); }, [])
   return (
     <div className="App">
-      <Header title="React" onChangeMode={function(){
+      <HeadTitle title="HI"></HeadTitle>
+      <Menu></Menu>
+      {/* <Nav topics={topics} onChangeMode={(id) => {
+        alert(id);
+      }}></Nav> */}
+      <TItleImg></TItleImg>
+      <Header title="React" onChangeMode={function () {
         alert("Heade2r");
       }}></Header>
-      <Header title="Js2"></Header>
-      <Header title="Vue"></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
-      }}></Nav>
-      <Article title="React" body="Js"></Article>
+
+      <h1 className="App-title">{message}</h1>
+      <p className="App-intro">                To get started, edit <code>src/App.js</code> and save to reload.            </p>
     </div >
+    
   );
 }
 
